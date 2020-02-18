ed57294e0548ea8e68b3462d3113b8a692b19b8a
Maxim Snoep <m.snoep@student.tue.nl>
2020-02-18 15:33:25 +0100

    Create SECRET.py

---
+++ SECRET.py
@@ -0,0 +1,16 @@
+#! python3
+# SECRET.py - Contains all secrets;
+
+# --- Canvas username and password.
+USERNAME = "USERNAME";
+PASSWORD = "PASSWORD";
+
+# --- URL of the assignment page.
+URL = "https://canvas.tue.nl/courses/COURSEID/assignments/ASSIGNMENTID";
+
+# --- Directory and files of assignment.
+DIR = "C:\\Users\\USER\\Documents\\PROJECT\\src\\"
+FILES = ["FILE1.java",
+         "FILE2.java",
+         "FILE3.java"
+         ]
