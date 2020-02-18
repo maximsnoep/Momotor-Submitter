1c80a175e768b7a750e7ca52280f29a35208eb78
Maxim Snoep <m.snoep@student.tue.nl>
2020-02-18 15:31:03 +0100

    First commit

---
+++ submitter.py
@@ -0,0 +1,33 @@
+#! python3
+# submitter.py - Submits files to Momotor.
+
+# --- Imports
+import SECRET
+from selenium import webdriver
+
+# --- Get secrets from SECRET.
+URL = SECRET.URL;
+USERNAME = SECRET.USERNAME;
+PASSWORD = SECRET.PASSWORD;
+DIR = SECRET.DIR;
+FILES = SECRET.FILES;
+
+# --- Initialize the WebDriver.
+browser = webdriver.Chrome(executable_path=r'chromedriver.exe');
+browser.get(URL);
+
+# --- Sign in, if not signed in yet.
+if browser.title == "Sign In":
+    browser.find_element_by_id("userNameInput").send_keys(USERNAME)
+    browser.find_element_by_id("passwordInput").send_keys(PASSWORD)
+    browser.find_element_by_id("submitButton").click()
+
+# --- Submit files.
+try:
+    browser.find_element_by_link_text("Submit Assignment").click()
+except:
+    browser.find_element_by_link_text("Re-submit Assignment").click()
+for index, file in enumerate(FILES):
+    browser.find_element_by_name("attachments[" + str(index) + "][uploaded_data]").send_keys(DIR + file)
+    browser.find_element_by_css_selector(".add_another_file_link").click()
+browser.find_element_by_id("submit_file_button").click()
