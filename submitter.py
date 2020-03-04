7c134fdf399176258d9db9b76d80ee60e85611d6
Maxim Snoep <m.snoep@student.tue.nl>
2020-03-04 15:16:21 +0100

    Update submitter.py
    fixed bug where it tried to click a button before it loaded.

--- submitter.py
+++ submitter.py
@@ -3,6 +3,9 @@
 
 # --- Importing imports. --- #
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import os, glob
 
@@ -39,10 +42,7 @@ all_files = []
 for file in files:
     all_files.extend(glob.glob(source_dir+file))
 print("    Submitting", len(all_files), "files...")
-try:
-    browser.find_element_by_link_text("Submit Assignment").click()
-except:
-    browser.find_element_by_link_text("Re-submit Assignment").click()
+WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'submit_assignment_link'))).click()
 for index, file in enumerate(all_files):
     browser.find_element_by_name("attachments[" + str(index) + "][uploaded_data]").send_keys(file)
     browser.find_element_by_css_selector(".add_another_file_link").click()
@@ -51,13 +51,12 @@ browser.find_element_by_id("submit_file_button").click()
 # Finish the submission by closing the WebDriver.
 print("    Finishing submission...")
 finished = False
-while not finished:
-    try:
-        browser.find_element_by_link_text("Re-submit Assignment")
-        browser.quit()
-        finished = True
-    except:
-        pass
-print("Submission finished!")
+try:
+    WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'submit_assignment_link')))
+    print("Submission finished!")
+except TimeoutException:
+    print("Submission failed!")
+finally:
+    browser.quit
 
 # --- End. --- #
